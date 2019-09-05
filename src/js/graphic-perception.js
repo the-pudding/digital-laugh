d0a0af16e9d39661a1d19013a43ff522e6e19309
russellgoldenberg <russellgoldenberg@gmail.com>
2019-09-05 15:29:30 -0400

    update dev version

--- src/js/graphic-perception.js
+++ src/js/graphic-perception.js
@@ -2,24 +2,63 @@
 import * as noUiSlider from 'nouislider';
 import db from './db';
 const $section = d3.select('#perception');
+const $ui = $section.select('.section__ui');
+const $figure = $section.select('.section__figure');
+const $scale = $ui.select('.ui__scale');
+const $slider = $ui.select('.ui__slider');
+const $termButton = $ui.selectAll('.ui__terms button');
+const $instructionsLaugh = $ui.select('.instructions--2 span');
 
 function resize() { }
 
-function setupSlider() {
-	const $slider = $section.select('.slider');
-
-	noUiSlider.create($slider.node(), {
-		start: [20, 80],
-		connect: true,
-		range: {
-			'min': 0,
-			'max': 100
-		}
-	});
+function handleTermClick() {
+	const $btn = d3.select(this);
+	const term = $btn.text();
+	$termButton.classed('is-active', false);
+	$btn.classed('is-active', true);
+	$slider.classed('is-disabled', false).attr('disabled', null);
+	$scale.classed('is-disabled', false);
+	$instructionsLaugh.text(`${term}`).classed('is-active', true);
 }
 
-function init() {
-	setupSlider();	
+function handleSliderChange([a, b]) {
+	const min = +a;
+	const max = +b;
+}
+
+function handleSliderSlide([a, b]) {
+	const min = +a;
+	const max = +b;
+	
+}
+
+function setupSlider() {
+	$slider.attr('disabled', true).classed('is-disabled', true);
+	$scale.classed('is-disabled', true);
+
+	const s = noUiSlider.create($slider.node(), {
+		start: [0, 10],
+		connect: true,
+		step: 1,
+		range: {
+			'min': 0,
+			'max': 10
+		},
+		// pips: {
+		// 	mode: 'steps',
+		// 	density: 10
+		// }
+	});
+
+	s.on('change', handleSliderChange);
+	s.on('slide', handleSliderSlide);
+}
+
+function setupTermButtons() {
+	$termButton.on('click', handleTermClick);
+}
+
+function setupDB() {
 	db.setup();
 	const returner = db.getReturner();
 	console.log({ returner })
@@ -29,7 +68,12 @@ function init() {
 	console.log(value)
 	// db.setReturner();
 	// db.finish();
-	
+}
+
+function init() {
+	setupSlider();
+	setupTermButtons();	
+	setupDB();
 }
 
 export default { init, resize };
