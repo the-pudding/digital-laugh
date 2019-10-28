4cd36fb2d70b9fd38bc7e8bb77ba77fcafabf7b2
russellgoldenberg <russellgoldenberg@gmail.com>
2019-10-28 12:45:22 -0400

    p3

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -2,20 +2,17 @@
 import Share from './part-share';
 import Lol from './part-lol';
 import Rank from './part-rank';
-import Review from './part-review';
 
 function resize() {
   Share.resize();
   Lol.resize();
   Rank.resize();
-  Review.resize();
 }
 
 function init() {
   Share.init();
   Lol.init();
   Rank.init();
-  Review.init();
 }
 
 export default { init, resize };
